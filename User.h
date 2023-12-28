9348e2bafbba0f000b7d98343daca3bb01195478
Muhammad Anas <muhammadanaseme@outlook.com>
2023-12-28 19:10:14 +0500

    added user class to handle the user

---
+++ User.h
@@ -0,0 +1,14 @@
+#pragma once
+//class for the user details
+class User
+{
+private:
+protected:
+public:
+	User(){}//constructor
+	void update_Profile(){}//update the user profile
+	void creat_Post(){}//create a post
+	void display_Posts(){}//display the posts of the user
+	void comment_on_Post(){}//comment on a post
+	~User(){}//destructor
+};
--- User_Manager.h
+++ User_Manager.h
@@ -6,8 +6,8 @@ private:
 protected:
 public:
 	User_Manager(){}//constructor
-	bool validate_User(){}//validates the user with initially stored data
-	bool create_User(){}//creates a new user
+	bool validate_User(){}//validates the user with initially stored data i.e for sign-in
+	bool create_User(){}//creates a new user i.e for sign-up
 	void get_User(){}//after sign-in or sign-up gets the user profile
 	~User_Manager(){}//destructor
 };

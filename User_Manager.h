ad24fd4272678b813040588b1010f2b2d8f72639
Muhammad Anas <muhammadanaseme@outlook.com>
2023-12-28 18:59:57 +0500

    outline of the usermanager class

---
+++ User_Manager.h
@@ -0,0 +1,13 @@
+#pragma once
+//class for handling the users
+class User_Manager
+{
+private:
+protected:
+public:
+	User_Manager(){}//constructor
+	bool validate_User(){}//validates the user with initially stored data
+	bool create_User(){}//creates a new user
+	void get_User(){}//after sign-in or sign-up gets the user profile
+	~User_Manager(){}//destructor
+};
